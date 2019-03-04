a95aced8a78d9730ce7b840ae33ff8a83e6fc682
jezzipan <jessica.leute@gmail.com>
2019-03-04 17:05:01 -0300

    exercicio de c

---
+++ index.c
@@ -0,0 +1,10 @@
+// 1-) Crie um programa que receba  um salario bruto e 
+// exiba um mensagem indicando se será descontado o teto do 
+// INSS. O desconto do teto do INSS ocorre quando 15% do salário
+// bruto ultrapassa $640,00. 
+
+
+// 2-) Crie um programa que receba um inteiro n(n>0)
+// e exiba a soma dos divisores de n. Exemplo:
+// n = 10 -> soma = 1 + 2 + 5 + 10 = 18 
+
